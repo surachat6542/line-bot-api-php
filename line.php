99d2b8a813ad77f77e03eb727b3099f142ad3545
surachat6542 <surachat6542@hotmail.com>
2019-07-12 14:57:11 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'NHrAxvnxcsT0hsKnoDzlYYqdAiODBV3RXCdnFc4SgYIiFDf+BleepXcBg08m2pz0ik07dwYblX4Jgf2qg8+26cq3W5AmSnRRgyufP8iVUH4kIZRTCiUEzcjnDc/W8itBF+UYkMDRM1i64VGU1r5V/AdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
